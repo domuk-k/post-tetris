eaf8456a6e443273a351dd6cd7d217522fd851d8
domuk-k <dannyworks102@gmail.com>
2021-03-18 01:08:55 +0900

    feat: initialize theme

---
+++ src/theme/index.js
@@ -0,0 +1,21 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  styles: {
+    global: props => ({
+      body: {
+        fontSize: 'sm',
+        lineHeight: 'tall',
+        boxSizing: 'border-box',
+      },
+      h1: {
+        fontWeight: 'bold',
+      },
+    }),
+  },
+  colors: {
+    cubes: 'B9B9B9',
+  },
+});
+
+export default theme;
