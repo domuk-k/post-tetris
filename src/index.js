e9d882753637a6892c7cd2c1c4b9d14c0ac67242
domuk-k <dannyworks102@gmail.com>
2021-03-18 01:58:40 +0900

    feat: add global store by redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import theme from './theme';
+import App from 'App';
+import theme from 'theme';
+import store from 'app/store';
 
 ReactDOM.render(
-  <ChakraProvider theme={theme}>
-    <App />
-  </ChakraProvider>,
+  <Provider store={store}>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </Provider>,
   document.getElementById('root'),
 );
