d9a7cd0a74edfdcbe37f1e54922e03dfd1fbee18
domuk-k <dannyworks102@gmail.com>
2021-03-19 11:21:46 +0900

    feat: add interface info UI

--- src/components/GameInfoSection.jsx
+++ src/components/GameInfoSection.jsx
@@ -1,15 +1,32 @@
-import { Flex } from '@chakra-ui/react';
-import { NEXT_BLOCK_SECTION } from 'utils/constants';
+import { Flex, Kbd } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+import { selectGame } from 'feature/game/gameSlice';
+import { NEXT_BLOCK_SECTION, format } from 'utils';
 import Board from 'feature/board/Board';
+import Matrix from './Matrix';
 
 const GameInfoSection = () => {
+  const {
+    score,
+    timer,
+    levelInfo: { stage },
+  } = useSelector(selectGame);
   return (
     <Flex as="section" direction="column" justifyContent="center" textAlign="center">
-      <GameInfo label="Score">2021</GameInfo>
-      <GameInfo label="Timer">01:33</GameInfo>
-      <GameInfo label="Level">1</GameInfo>
+      <GameInfo label="Score">{score}</GameInfo>
+      <GameInfo label="Timer">{format(timer)}</GameInfo>
+      <GameInfo label="Level">{stage}</GameInfo>
       <GameInfo label="Next Block">
-        {/* <Board matrix={NEXT_BLOCK_SECTION} /> */}
+        <Matrix matrix={NEXT_BLOCK_SECTION} />
+      </GameInfo>
+      <GameInfo label="Interfaces">
+        <Flex direction="column" w="80%" alignItems="center" margin="0 auto">
+          <Kbd>ArrowDown</Kbd>
+          <Kbd>ArrowLeft</Kbd>
+          <Kbd>ArrowRight</Kbd>
+          <Kbd>SpaceBar</Kbd>
+          <div>and dummy mousemove</div>
+        </Flex>
       </GameInfo>
     </Flex>
   );
@@ -17,10 +34,10 @@ const GameInfoSection = () => {
 
 const GameInfo = ({ label, children }) => {
   return (
-    <div>
+    <section>
       <h1>{label}</h1>
       {children}
-    </div>
+    </section>
   );
 };
 
